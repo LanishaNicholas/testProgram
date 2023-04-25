079ee5ab1a22819d0e1aaca63ca21a28af74a1a1
Lanisha Nicholas <88057783+LanishaNicholas@users.noreply.github.com>
2023-04-25 10:19:54 +0200

    Key update

--- openai-test/index.php
+++ openai-test/index.php
@@ -5,7 +5,7 @@ require 'src/OpenAi.php';
 require 'src/Url.php';
 use Orhanerday\OpenAi\OpenAi;
 
-$open_ai = new OpenAi('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');
+$open_ai = new OpenAi('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');
 
 //$client = OpenAI::client('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');
 
