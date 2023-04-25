5ac14b7626730f7e4ab944c87b18192c57df6826
Lanisha Nicholas <88057783+LanishaNicholas@users.noreply.github.com>
2023-04-25 10:41:07 +0200

    key update

--- openai-test/index.php
+++ openai-test/index.php
@@ -5,7 +5,7 @@ require 'src/OpenAi.php';
 require 'src/Url.php';
 use Orhanerday\OpenAi\OpenAi;
 
-$open_ai = new OpenAi('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');
+//$open_ai = new OpenAi('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');
 
 //$client = OpenAI::client('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');
 
@@ -13,8 +13,36 @@ $prompt = <<<TEXT
 	Svalbard, en øygruppe i Arktis, er kanskje mest kjent for sin villmark, sitt dyreliv og sinepolarekspedisjoner. Men det er også et sted hvor kultur og underholdning trives, og klovnerhar spilt en spesiell rolle i Svalbards kulturelle landskap. Denne undervisningsartikkelen tarfor seg historien, betydningen og kunsten bak klovneriet i Svalbard, og gir en innføring ihvordan denne særegne underholdningsformen har utviklet seg på øygruppen.Klovneriets historie i Svalbard kan spores tilbake til tidlige europeiske oppdagelsesreisende og hvalfangere som kom til øygruppen på 1600- og 1700-tallet. Disse sjøfolkene brakte medseg ulike kulturelle uttrykk, og klovneriet ble introdusert som en form for underholdning forå bryte opp monotonien i det isolerte arktiske livet. Klovneforestillinger ble derfor en viktigsosial aktivitet og et middel for samhold blant de tidlige bosetterne.Klovneriet har hatt en spesiell betydning for Svalbard, da det har fungert som et kultureltbindeledd mellom folk fra forskjellige nasjonaliteter og bakgrunner som har bosatt seg på øygruppen. Gjennom humor, slapstick og fysiske krumspring har klovner vært i stand til å formidle historier og budskap som har krysset språk- og kulturgrenser. Dette har bidratt tilå skape en felles identitet og et kulturelt fellesskap for Svalbards innbyggere.I dag er klovneriet en integrert del av Svalbards kulturelle landskap, og det finnes en rekke organiserte grupper og ensembler som fremfører klovneforestillinger både lokalt og internasjonalt. Klovner i Svalbard er også engasjert i ulike sosiale og miljømessige initiativer, som bidrar til å styrke samfunnet og øke bevisstheten rundt øygruppens unike utfordringer.Klovneriet i Svalbard er et levende og mangfoldig kulturelt fenomen som har en lang historie og en dyp betydning for øygruppens innbyggere. Klovner har ikke bare fungert som underholdere, men også som kulturelle brobyggere og samfunnsengasjerte aktører. Ved å lære om klovner i Svalbard, får vi et unikt innblikk i et samfunn som har klart å forene ulike kulturer og tradisjoner gjennom kunsten og kraften av humor.
 TEXT;
 
-$complete = $open_ai->completion(
-    // object with all parameters
+$api_key = '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';
+$ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/completions');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    $postFields = '{
+            "model": "text-ada-001",
+            "prompt": "Please write a one paragraph professional synopsis: ' . $prompt . '",
+            "temperature": 0,
+            "max_tokens": 500,
+            "top_p": 1,
+            "frequency_penalty": 0,
+            "presence_penalty": 0
+        }';
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+
+    $headers = array();
+    $headers[] = 'Content-Type: application/json';
+    $headers[] = 'Authorization: Bearer ' . $api_key;
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+    curl_close($ch);
+    echo '<pre>'; print_r($result);die;
+/*$complete = $open_ai->completion(
+    
 	[
     'model' => 'text-davinci-003',
     'prompt' => $prompt,
@@ -27,7 +55,7 @@ $complete = $open_ai->completion(
 	]
 
 , function($curl_info, $data){
-    // response
+   
 	echo $data;
 	$obj = json_decode($data,true);
 	echo $obj["id"];
@@ -44,7 +72,7 @@ if (isset($_POST['submit'])) {
     
     //$response = generate_response($prompt);
     
-}
+}*/
 ?>
 <!DOCTYPE html>
 <html>
