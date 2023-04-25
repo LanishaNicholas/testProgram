5d323e7fa6c3a4933c2d330c1f2fc12c511ecf4f
Lanisha Nicholas <88057783+LanishaNicholas@users.noreply.github.com>
2023-04-25 09:03:16 +0200

    Add html part

--- openai-test/index.php
+++ openai-test/index.php
@@ -5,14 +5,13 @@ require 'src/OpenAi.php';
 require 'src/Url.php';
 use Orhanerday\OpenAi\OpenAi;
 
-$open_ai = new OpenAi('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');
+$open_ai = new OpenAi('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');
 //$prompt = $_GET['prompt'];
 
 
 //$client = OpenAI::client('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');
 
 $prompt = <<<TEXT
-	Tittel: Klovner på Svalbard: En fargerik og kulturell undervisningsartikkel
 	Svalbard, en øygruppe i Arktis, er kanskje mest kjent for sin villmark, sitt dyreliv og sinepolarekspedisjoner. Men det er også et sted hvor kultur og underholdning trives, og klovnerhar spilt en spesiell rolle i Svalbards kulturelle landskap. Denne undervisningsartikkelen tarfor seg historien, betydningen og kunsten bak klovneriet i Svalbard, og gir en innføring ihvordan denne særegne underholdningsformen har utviklet seg på øygruppen.Klovneriets historie i Svalbard kan spores tilbake til tidlige europeiske oppdagelsesreisende og hvalfangere som kom til øygruppen på 1600- og 1700-tallet. Disse sjøfolkene brakte medseg ulike kulturelle uttrykk, og klovneriet ble introdusert som en form for underholdning forå bryte opp monotonien i det isolerte arktiske livet. Klovneforestillinger ble derfor en viktigsosial aktivitet og et middel for samhold blant de tidlige bosetterne.Klovneriet har hatt en spesiell betydning for Svalbard, da det har fungert som et kultureltbindeledd mellom folk fra forskjellige nasjonaliteter og bakgrunner som har bosatt seg på øygruppen. Gjennom humor, slapstick og fysiske krumspring har klovner vært i stand til å formidle historier og budskap som har krysset språk- og kulturgrenser. Dette har bidratt tilå skape en felles identitet og et kulturelt fellesskap for Svalbards innbyggere.I dag er klovneriet en integrert del av Svalbards kulturelle landskap, og det finnes en rekke organiserte grupper og ensembler som fremfører klovneforestillinger både lokalt og internasjonalt. Klovner i Svalbard er også engasjert i ulike sosiale og miljømessige initiativer, som bidrar til å styrke samfunnet og øke bevisstheten rundt øygruppens unike utfordringer.Klovneriet i Svalbard er et levende og mangfoldig kulturelt fenomen som har en lang historie og en dyp betydning for øygruppens innbyggere. Klovner har ikke bare fungert som underholdere, men også som kulturelle brobyggere og samfunnsengasjerte aktører. Ved å lære om klovner i Svalbard, får vi et unikt innblikk i et samfunn som har klart å forene ulike kulturer og tradisjoner gjennom kunsten og kraften av humor.
 TEXT;
 
@@ -32,16 +31,24 @@ $complete = $open_ai->completion(
 , function($curl_info, $data){
     // response
 	echo $data;
+	//$decoded = json_decode($data,true);
+	//echo $decoded;
 	//echo PHP_EOL;
 	//ob_flush();
 	//flush();
 	//return strlen($data);
 
 });
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My page</title>
+	</head>
+	<body>
+		<h2>Tittel: Klovner på Svalbard: En fargerik og kulturell undervisningsartikkel</h2>
+		<p><?php echo $prompt; ?></p>
+		<button id="summaryButton">Show the summary</button>
+	</body>
+</html>
 
-/*$result = $client->completions()->create([
-    'model' => 'text-davinci-003', // The most expensive one, but the best.
-    'prompt' => $prompt,
-]);*/
- 
-//echo $result['choices'][0]['text'];
